cc1a9c4734ecf702ee7da4666507ba1f69f84f36
Pradipta Sarker <pradiptasarker@Pradiptas-MacBook-Pro.local>
2024-11-11 22:45:21 +0600

    day update after some long time, no ideas about incident of life

--- Day_01/Project_01_[Expanding_Cards]/main.js
+++ Day_01/Project_01_[Expanding_Cards]/main.js
@@ -2,6 +2,16 @@ const panels = document.querySelectorAll('.panel');
 
 // console.log(panels);
 
+// remove the active class
+
+function removeActiveClasses(){
+    panels.forEach( panel =>{
+        panel.classList.remove('active');
+    })
+}
+
+// remove the active class and initial the current panel active
+
 panels.forEach((panel)=>{
     // console.log(panel);
     panel.addEventListener('click',()=>{
@@ -11,8 +21,3 @@ panels.forEach((panel)=>{
 })
 
 
-function removeActiveClasses(){
-    panels.forEach( panel =>{
-        panel.classList.remove('active');
-    })
-}
---
+++ Day__02/Learn.md
@@ -0,0 +1,21 @@
+    #Reading the Basic
+
+    ## some Notes ###
+
+    Output
+
+    Statement
+
+    Syntax
+
+    Comments
+
+    Variable
+
+    Let Vs Const Vs Var
+
+    Operators [arithmetic ... ]
+
+    Data Types
+
+    Function
