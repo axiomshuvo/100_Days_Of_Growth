7bc3a80d5c22d5e92a5264aa868136685d4ec7e1
Pradipta Sarker <pradiptasarker@Pradiptas-MacBook-Pro.local>
2025-01-11 15:46:46 +0600

    some update i don't remember

--- JS_Revisit_/object.js
+++ JS_Revisit_/object.js
@@ -3,7 +3,7 @@
 
 # In real life, objects are things like: houses, cars, people, animals, or any other subjects.
 # A real life car has properties like weight and color:
-@ Car objects have the same properties, but the values differ from car to car.
+@ example: a Car objects have the same properties, but the values differ from car to car.
 
 
 # A real life car has methods like start and stop:
@@ -122,7 +122,7 @@ person.eyeColor = "blue";
     $ Accessing Object Properties
 
     #You can access object properties in two ways:
-
+    by concatenating the object name & property name together and other way as array index 
 */
 
     objectName.propertyName
@@ -136,4 +136,84 @@ person.eyeColor = "blue";
     # Methods are function definitions stored as property values.
     
     
+*/
+
+const person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+/*
+? In the example above, this refers to the person object:
+
+@ this.firstName means the firstName property of person.
+@ this.lastName means the lastName property of person.
+
+
+*/
+
+
+/*
+! In JavaScript, Objects are King.
+
+# Objects are containers for Properties and Methods.
+# Properties are named Values.
+# Methods are Functions stored as Properties.
+# Properties can be primitive values, functions, or even other objects.
+
+*/
+
+/*
+? All JavaScript values, except primitives, are objects.
+! JavaScript Primitives
+
+@ A primitive value is a value that has no properties or methods.
+# A primitive data type is data that has a primitive value.
+ #JavaScript defines 7 types of primitive data types:
+        string
+        number
+        boolean
+        null
+        undefined
+        symbol
+        bigint
+
+! Immutable
+
+? Primitive values are immutable (they are hardcoded and cannot be changed).
+@ if x = 3.14, you can change the value of x, but you cannot change the value of 3.14.
+
+*/
+
+
+/* 
+
+! JavaScript Objects are Mutable
+
+? Objects are mutable: They are addressed by reference, not by value.
+
+*/
+
+//Create an Object
+const person = {
+  firstName:"John",
+  lastName:"Doe",
+  age:50, eyeColor:"blue"
+}
+
+// Create a copy
+const x = person;
+
+// Change Age in both
+x.age = 10;
+
+
+/*
+# The object x is not a copy of person. The object x is person.
+? The object x and the object person share the same memory address.
+@ Any changes to x will also change person:
 */
