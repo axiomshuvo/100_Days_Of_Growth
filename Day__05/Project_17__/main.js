9c534b45ac465a067b8837b65db59a52eadbb32e
Pradipta Sarker <techshuvo@gmail.com>
2025-03-11 14:15:26 +0600

    pro-17 api fetch, filter

--- Day__05/Project_17__/main.js
+++ Day__05/Project_17__/main.js
@@ -1,2 +1,70 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie/popular&api_key=1f60d5995b3686d00b7d2fe9eff64bb6&page=1';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=96b595e40fc61309314c8a1e83652947&page=1'
 
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+const SEARCH_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=96b595e40fc61309314c8a1e83652947&query="';
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+const main = document.getElementById('main');
+
+
+getMovies(API_URL);
+
+
+async function getMovies(url){
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.results);
+    showMovies(data.results);
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm && searchTerm !== ''){
+        getMovies(SEARCH_URL + searchTerm );
+        console.log(SEARCH_URL + searchTerm);
+        search.value = '';
+    }else{
+        window.location.reload();
+    }
+});
+
+function showMovies(movies){
+    main.innerHTML = '';
+    movies.forEach(movie => {
+        const {title,poster_path,vote_average,overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `<img
+          src="${IMG_PATH+poster_path}"
+          alt="${title}"
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+          </div>
+          `;
+          main.appendChild(movieEl);
+});
+}
+
+
+function getClassByRate(vote) {
+    if(vote >= 8){
+        return 'green';
+    }else if( vote >= 5){
+        return "orange"
+    }else{
+        return "red";
+    }
+    
+}
